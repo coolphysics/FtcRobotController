3f838ea8e98a34e83c291c5eaa37b6e328597e60
Friars23707 <147444129+Friars23707@users.noreply.github.com>
2025-10-13 10:40:42 -0400

    New Method Practice

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamMemberPracticeTwo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamMemberPracticeTwo.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
-public class TeamMemberPractice extends OpMode {
+public class TeamMemberPracticeTwo extends OpMode {
 
     boolean initDone:
 
@@ -37,3 +37,4 @@ public class TeamMemberPractice extends OpMode {
 
     }
 }
+
