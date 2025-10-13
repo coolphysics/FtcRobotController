4fb2e5e87fd102f73bf4c47f57484a5db071a384
Friars23707 <147444129+Friars23707@users.noreply.github.com>
2025-10-13 10:36:40 -0400

    Update teamMemberPractice.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamMemberPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamMemberPractice.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
-public class teamMemberPractice extends OpMode {
+public class TeamMemberPractice extends OpMode {
 
     boolean initDone:
 
